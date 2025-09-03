dc95d05ae3995030aa28f8a92d7d2ef12570277f
Erik Isaksson <erikis@kth.se>
2025-09-03 17:13:21 +0200

    Style fixes

--- instances-process.py
+++ instances-process.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # Source: https://github.com/erikis/instances-script
 # MIT License
 #
@@ -21,6 +21,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+"""Read instances JSON and output hosts and nftables files"""
 import json
 import sys
 import os
@@ -31,12 +32,15 @@ import filelock
 # Debian requirements: apt install python3 python3-filelock
 
 def main():
-    parser = argparse.ArgumentParser(description="Read instances JSON (default path /var/lib/misc/instances.json) and output hosts and nftables files")
-    parser.add_argument('-f', '--force', action='store_true', help="process even if an update is not detected")
-    
+    """Read instances JSON and output hosts and nftables files"""
+    parser = argparse.ArgumentParser(description="Read instances JSON (default path \
+            /var/lib/misc/instances.json) and output hosts and nftables files")
+    parser.add_argument('-f', '--force', action='store_true',
+            help="process even if an update is not detected")
+
     args = parser.parse_args()
     is_forced = args.force
-    
+
     # Figure out all file paths
     file_prefix = os.environ.get('INSTANCES_BASE_PATH', default='/var/lib/misc/instances')
     file_suffix = ''
@@ -51,7 +55,7 @@ def main():
     nftables_path = f'{file_prefix}{file_suffix}.nftables'
     updated_path = f'{file_prefix}{file_suffix}.updated'
     lock_path = f'{file_prefix}{file_suffix}.lock'
-    
+
     # Acquire a file lock and start processing
     lock = filelock.FileLock(lock_path, timeout=10)
     lock.acquire()
@@ -73,11 +77,13 @@ def main():
     finally:
         lock.release()
 
+ENC = 'utf-8'
+
 def load_instances_json(file_path):
     """Load the instances JSON file, return empty dict if file doesn't exist"""
     if os.path.exists(file_path):
         try:
-            with open(file_path, 'r') as f:
+            with open(file_path, 'r', encoding=ENC) as f:
                 instances = json.load(f)
         except (json.JSONDecodeError, IOError) as e:
             print(f"Error reading JSON file: {e}", file=sys.stderr)
@@ -85,39 +91,39 @@ def load_instances_json(file_path):
     else:
         instances = {}
     return instances
-    
+
 def save_instances_hosts(file_path, instances):
     """Save the host addresses to file"""
     count = 0
-    instance_domain = os.environ.get('INSTANCES_HOSTS_DOMAIN', default='.instance.internal')
-    if not re.fullmatch(r'[a-zA-Z0-9\.-]*', instance_domain):
-        print(f"Invalid hosts domain: {instance_domain}", file=sys.stderr)
+    domain = os.environ.get('INSTANCES_HOSTS_DOMAIN', default='.instance.internal')
+    if not re.fullmatch(r'[a-zA-Z0-9\.-]*', domain):
+        print(f"Invalid hosts domain: {domain}", file=sys.stderr)
         return count
     try:
-        with open(file_path, 'w') as f:
+        with open(file_path, 'w', encoding=ENC) as f:
             ip_address_fields = ['ipv4', 'ipv6_gua', 'ipv6_ula', 'ipv6_lla']
-            for mac, instance in instances.items():
+            for _, instance in instances.items():
                 name = instance.get('name')
                 if name:
                     for ip_address_field in ip_address_fields:
                         ip_address = instance.get(ip_address_field)
                         if ip_address:
                             # Write <ip-address> <full-name> <extra-names>
-                            full_name = f'{name}{instance_domain}'
+                            full_name = f'{name}{domain}'
                             extra_names = ''
                             match ip_address_field:
                                 case 'ipv4':
                                     # Extra name for only resolving to IPv4 address
-                                    extra_names = f' {name}.v4{instance_domain}'
+                                    extra_names = f' {name}.v4{domain}'
                                 case 'ipv6_gua':
                                     # Extra names for resolving to IPv6 globally reachable address
-                                    extra_names = f' {name}.v6{instance_domain} {name}.g6{instance_domain}'
+                                    extra_names = f' {name}.v6{domain} {name}.g6{domain}'
                                 case 'ipv6_ula':
                                     # Extra names for resolving to IPv6 unique local address
-                                    extra_names = f' {name}.v6{instance_domain} {name}.u6{instance_domain}'
+                                    extra_names = f' {name}.v6{domain} {name}.u6{domain}'
                                 case 'ipv6_lla':
                                     # Ensure IPv6 link-local address is only used if asked for
-                                    full_name = f'{name}.l6{instance_domain}'
+                                    full_name = f'{name}.l6{domain}'
                             f.write(f'{ip_address} {full_name}{extra_names}\n')
                             count += 1
     except IOError as e:
@@ -127,24 +133,24 @@ def save_instances_hosts(file_path, instances):
 
 def save_instances_nftables(file_path, instances):
     """Save the nftables rules and sets to files"""
-    
+
     # Check which sets to include: comma-separated hostnames in INSTANCES_NFTABLES_ADDRESS_SETS
     all_addresses = { 'v4': [], 'v6': [], 'g6': [], 'u6': [], 'l6': [] }
-    address_maps = { None: all_addresses } # Use None and not empty string because instance names might be empty
+    address_maps = { None: all_addresses } # Use None because instance names might be empty string
     for address_set in os.environ.get('INSTANCES_ADDRESS_SETS', default='host').split(','):
         name = address_set.strip()
         if name:
-            if re.fullmatch(r'[a-zA-Z][a-zA-Z0-9-]*', name): # Same regex as for hostname in instances-update.py
+            if re.fullmatch(r'[a-zA-Z][a-zA-Z0-9-]*', name): # Name regex from instances-update.py
                 name = name.replace('-', '_') # Hyphen not allowed in nftables identfier
                 address_maps[name] = { key: [] for key, value in all_addresses.items() }
             else:
                 # Print the error but continue, ignoring the invalid hostname
                 print(f"Invalid hostname for address set: {name}", file=sys.stderr)
-    
+
     # Write the rules file and also collect addresses for the sets
     count = 0
     try:
-        with open(f'{file_path}_chains', 'w') as f:
+        with open(f'{file_path}_chains', 'w', encoding=ENC) as f:
             f.write('# Use: ether type arp jump instances_drop_arp\n')
             f.write('chain instances_drop_arp {\n')
             for mac_address, instance in instances.items():
@@ -155,15 +161,15 @@ def save_instances_nftables(file_path, instances):
                     comment = ''
                 ip_address = instance.get('ipv4')
                 if ip_address:
-                    f.write(f'    arp saddr ip {ip_address} counter ether saddr {mac_address} counter return{comment}\n')
+                    f.write(f'    arp saddr ip {ip_address} counter ether saddr {mac_address} counter return{comment}\n') # pylint: disable=line-too-long
                     count += 1
                     for address_map in [all_addresses, address_maps.get(name)]:
                         if address_map:
                             address_map['v4'].append(ip_address)
-            f.write('    counter drop comment "lockdown" # prepend to log to dmesg: log prefix "[nftables] dropped ARP: "\n')
+            f.write('    counter drop comment "lockdown" # prepend to log to dmesg: log prefix "[nftables] dropped ARP: "\n') # pylint: disable=line-too-long
             count += 1
             f.write('}\n')
-            f.write('# Use: ether type ip6 icmpv6 type nd-neighbor-advert jump instances_drop_ndp\n')
+            f.write('# Use: ether type ip6 icmpv6 type nd-neighbor-advert jump instances_drop_ndp\n') # pylint: disable=line-too-long
             f.write('chain instances_drop_ndp {\n')
             ip_address_fields = ['ipv6_gua', 'ipv6_ula', 'ipv6_lla']
             for mac_address, instance in instances.items():
@@ -172,14 +178,13 @@ def save_instances_nftables(file_path, instances):
                     comment = f' comment "{name}"'
                 else:
                     comment = ''
-                mac = mac_address.replace(':', '')
                 for ip_address_field in ip_address_fields:
                     ip_address = instance.get(ip_address_field)
                     if ip_address:
                         ip_hex = ipaddress.ip_address(ip_address).exploded.replace(':', '')
                         # See RFC 4861 "Neighbor Advertisement Message Format":
                         # Bit 384 of IPv6 packet = bit 64 of NA message = start of Target Address
-                        f.write(f'    @nh,384,128 0x{ip_hex} counter ether saddr {mac_address} counter return{comment}\n')
+                        f.write(f'    @nh,384,128 0x{ip_hex} counter ether saddr {mac_address} counter return{comment}\n') # pylint: disable=line-too-long
                         count += 1
                         for address_map in [all_addresses, address_maps.get(name)]:
                             if address_map:
@@ -192,17 +197,17 @@ def save_instances_nftables(file_path, instances):
                                             address_map['u6'].append(ip_address)
                                 else:
                                     address_map['l6'].append(ip_address)
-            f.write('    counter drop comment "lockdown" # prepend to log to dmesg: log prefix "[nftables] dropped NDP: "\n')
+            f.write('    counter drop comment "lockdown" # prepend to log to dmesg: log prefix "[nftables] dropped NDP: "\n') # pylint: disable=line-too-long
             count += 1
             f.write('}\n')
     except IOError as e:
         print(f"Error writing nftables file: {e}", file=sys.stderr)
         sys.exit(1)
-    
+
     # Write the sets file with naming similar to that of the hosts file
     count_sets = 0
     try:
-        with open(f'{file_path}_sets', 'w') as f:
+        with open(f'{file_path}_sets', 'w', encoding=ENC) as f:
             for address_name, address_map in address_maps.items():
                 for address_type, ip_addresses in address_map.items():
                     if address_name:
@@ -226,7 +231,7 @@ def save_instances_nftables(file_path, instances):
     except IOError as e:
         print(f"Error writing nftables_sets file: {e}", file=sys.stderr)
         sys.exit(1)
-    
+
     return count, count_sets
 
 if __name__ == '__main__':
--- instances-update.py
+++ instances-update.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # Source: https://github.com/erikis/instances-script
 # MIT License
 #
@@ -21,6 +21,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+"""Update, add, or remove an instance in instances JSON"""
 import json
 import sys
 import os
@@ -33,13 +34,20 @@ import filelock
 # Debian requirements: apt install python3 python3-filelock jq
 
 def main():
-    parser = argparse.ArgumentParser(add_help=False, prefix_chars=[None], description="Update, add, or remove an instance in instances JSON (default path /var/lib/misc/instances.json)")
-    parser.add_argument('action', help="dnsmasq dhcp-script action, or special action: --initialize, --rename, --remove, --help")
-    parser.add_argument('mac_address', nargs='?', default=None, help="MAC address (if IPv6 then ignored and DNSMASQ_MAC is used), or interface (e.g., br0) if action is --initialize")
-    parser.add_argument('ip_address', nargs='?', default=None, help="IPv4 or IPv6 address, or name if action is --initialize or --rename, or not used if action is --remove")
+    """Update, add, or remove an instance in instances JSON"""
+    parser = argparse.ArgumentParser(add_help=False, prefix_chars=[None],
+            description="Update, add, or remove an instance in instances JSON \
+            (default path /var/lib/misc/instances.json)")
+    parser.add_argument('action', help="dnsmasq dhcp-script action, or special action: \
+            --initialize, --rename, --remove, --help")
+    parser.add_argument('mac_address', nargs='?', default=None, help="MAC address (if IPv6 then \
+            ignored and DNSMASQ_MAC is used), or interface (e.g., br0) if action is --initialize")
+    parser.add_argument('ip_address', nargs='?', default=None, help="IPv4 or IPv6 address, or \
+            name if action is --initialize or --rename, or not used if action is --remove")
     parser.add_argument('hostname', nargs='?', default=None, help="name (only used by dnsmasq)")
-    parser.add_argument('ignored', nargs='*', default=None, help="extra arguments for ignored actions (only used by dnsmasq)")
-    
+    parser.add_argument('ignored', nargs='*', default=None, help="extra arguments for ignored \
+            actions (only used by dnsmasq)")
+
     # When executed as dhcp-script there are only positional arguments, and in case they ever start
     # with --/-, parse without (valid) prefix_chars so that --/- options are effectively disabled.
     # Instead, use -- as prefix for special actions to avoid future conflicts with dnsmasq actions.
@@ -49,18 +57,18 @@ def main():
     ip_address = None
     hostname = None
     interface_name = None
-    
-    def check_arg_count(mini, maxi): # Action is always required but other arguments depend on the action
+
+    def check_arg_count(mini, maxi): # Action is always required but others depend on the action
         count = len(sys.argv) - 1 # Don't count the script name
         if (not mini is None and count < mini) or (not maxi is None and count > maxi):
             print(f"Wrong number of arguments for action {action}, see --help", file=sys.stderr)
             sys.exit(1)
-    
+
     match action:
         case 'add' | 'old':
             # Executed by dnsmasq for lease creation (add) by using the dnsmasq option
             # --dhcp-script with the path to this script and on renewal (old) with the option
-            # --script-on-renewal. Also invoked on dnsmasq startup and on HUP signal. 
+            # --script-on-renewal. Also invoked on dnsmasq startup and on HUP signal.
             check_arg_count(3, None) # No maximum number of arguments in case dnsmasq adds more
             try:
                 ip_address = ipaddress.ip_address(args.ip_address)
@@ -68,7 +76,7 @@ def main():
                 print(f"Invalid IP address: {args.ip_address}", file=sys.stderr)
                 sys.exit(1)
             # For IPv6 the MAC address is instead provided in an environment variable
-            if type(ip_address) is ipaddress.IPv6Address:
+            if isinstance(ip_address, ipaddress.IPv6Address):
                 mac_address = os.environ.get('DNSMASQ_MAC') # "MAC address of the client, if known"
                 if not mac_address:
                     print("DNSMASQ_MAC environment variable not set", file=sys.stderr)
@@ -87,7 +95,6 @@ def main():
             # In this case the name is given as ip_address (ip_address will remain None).
             check_arg_count(3, 3)
             hostname = args.ip_address
-            pass
         case '--remove':
             # Special action for removing an instance identified by mac_address.
             # Only mac_address is given (hostname and ip_address will remain None).
@@ -104,7 +111,7 @@ def main():
             # normal operation the instances JSON file is regarded as append-only. If an instance
             # needs to be removed then use the special 'remove' action.
             sys.exit(0)
-    
+
     # Roughly validate mac_address, hostname, interface_name
     mac_address_re = r'([0-9a-f]{2}:){5}[0-9a-f]{2}'
     if not mac_address is None and not re.fullmatch(mac_address_re, mac_address):
@@ -116,7 +123,7 @@ def main():
     if not hostname is None and not re.fullmatch(r'[a-zA-Z][a-zA-Z0-9-]*', hostname):
         print(f"Invalid hostname: {hostname}", file=sys.stderr)
         sys.exit(1)
-        
+
     # Figure out all file paths
     file_prefix = os.environ.get('INSTANCES_BASE_PATH', default='/var/lib/misc/instances')
     file_suffix = ''
@@ -129,7 +136,7 @@ def main():
     file_path = f'{file_prefix}{file_suffix}.json'
     updated_path = f'{file_prefix}{file_suffix}.updated'
     lock_path = f'{file_prefix}{file_suffix}.lock'
-    
+
     # While for dhcp-script "at most one instance of the script is ever running", the script can
     # also be executed manually, and the JSON file is read by instances-process.py, so there might
     # be concurrent access and a file lock is necessary.
@@ -144,12 +151,13 @@ def main():
                 print(f"Couldn't get MAC address for interface {interface_name}", file=sys.stderr)
                 sys.exit(1)
             elif not re.fullmatch(mac_address_re, mac_address):
-                print(f"Invalid MAC address for interface {interface_name}: {mac_address}", file=sys.stderr)
+                print(f"Invalid MAC address for interface {interface_name}: {mac_address}", \
+                        file=sys.stderr)
                 sys.exit(1) # In case of bad data (see load_instances_json)
-        
+
         # Update or add the instance
         changes_made = update_instance(instances, mac_address, ip_address, hostname)
-        
+
         # Save if changes were made
         if changes_made:
             save_instances_json(file_path, instances)
@@ -158,20 +166,21 @@ def main():
     finally:
         lock.release()
 
+ENC = 'utf-8'
 ULA = ipaddress.IPv6Network('fc00::/7') # RFC 4193 Unique Local IPv6 Unicast Addresses
 
 def load_instances_json(interface_name, file_path):
     """Load the instances JSON file, return empty dict if file doesn't exist"""
     if os.path.exists(file_path):
         try:
-            with open(file_path, 'r') as f:
+            with open(file_path, 'r', encoding=ENC) as f:
                 instances = json.load(f)
         except (json.JSONDecodeError, IOError) as e:
             print(f"Error reading JSON file: {e}", file=sys.stderr)
             sys.exit(1)
     else:
         instances = {}
-    
+
     # For the special 'initialize' action an interface name is provided. Use it to create (or
     # update) an instance for the interface, with info equivalent to that of other instances.
     if interface_name:
@@ -180,11 +189,12 @@ def load_instances_json(interface_name, file_path):
         results = []
         commands = [
             f"ip -json address show dev \"{interface_name}\" | jq -r '.[] | .address'",
-            f"ip -json address show dev \"{interface_name}\" -secondary | jq -r '.[] | .addr_info[] | select(.family==\"inet\") | .local'",
-            f"ip -json address show dev \"{interface_name}\" -temporary | jq -r '.[] | .addr_info[] | select(.family==\"inet6\") | .local'"
+            f"ip -json address show dev \"{interface_name}\" -secondary | jq -r '.[] | .addr_info[] | select(.family==\"inet\") | .local'",  # pylint: disable=line-too-long
+            f"ip -json address show dev \"{interface_name}\" -temporary | jq -r '.[] | .addr_info[] | select(.family==\"inet6\") | .local'"  # pylint: disable=line-too-long
         ]
         for command in commands:
-            result = subprocess.run(command, shell=True, capture_output=True, text=True)
+            result = subprocess.run(command, shell=True, capture_output=True, text=True,
+                    check=False)
             results.append(result.stdout.splitlines())
         if len(results[0]) != 1:
             # Unable to get the MAC address -- wrong interface name?
@@ -217,7 +227,7 @@ def load_instances_json(interface_name, file_path):
 def save_instances_json(file_path, instances):
     """Save the instances JSON to file"""
     try:
-        with open(file_path, 'w') as f:
+        with open(file_path, 'w', encoding=ENC) as f:
             json.dump(instances, f, indent=2)
     except IOError as e:
         print(f"Error writing JSON file: {e}", file=sys.stderr)
@@ -225,7 +235,7 @@ def save_instances_json(file_path, instances):
 
 def update_instance(instances, mac_address, ip_address, hostname):
     """Update or add instance in the instances dictionary"""
-    
+
     # Find or create the instance
     updated = False
     if mac_address not in instances:
@@ -243,7 +253,7 @@ def update_instance(instances, mac_address, ip_address, hostname):
                     name = ''
         # Calculate the EUI-64 IPv6 link-local address based on the MAC address
         # Following RFC 4291 Appendix A:
-        # 1. Invert universal/local bit (bit 7) (^2 = XOR 2nd bit, [2:] = skip '0x') 
+        # 1. Invert universal/local bit (bit 7) (^2 = XOR 2nd bit, [2:] = skip '0x')
         # 2. Insert two octets 0xFF and 0xFE in the middle of the 48-bit MAC address
         mac_hex = mac_address.replace(':', '') # Assume already lower-case
         eui64 = f'{mac_hex[0:1]}{hex(int(mac_hex[1:2],16)^2)[2:]}{mac_hex[2:6]}fffe{mac_hex[6:]}'
@@ -256,7 +266,7 @@ def update_instance(instances, mac_address, ip_address, hostname):
         updated = True
     else:
         instance = instances[mac_address]
-    
+
     # Handle special actions (ip_address is None)
     if ip_address is None:
         if hostname is None:
@@ -274,10 +284,10 @@ def update_instance(instances, mac_address, ip_address, hostname):
                     if ins.get('name') == hostname:
                         ins['name'] = ''
                         updated = True
-    
+
     # Update IP address field based on address type
     else:
-        if type(ip_address) is ipaddress.IPv6Address:
+        if isinstance(ip_address, ipaddress.IPv6Address):
             ip_network = ipaddress.ip_network(int(ip_address))
             if ip_address.is_global:
                 if instance.get('ipv6_gua') != str(ip_address):
@@ -287,11 +297,11 @@ def update_instance(instances, mac_address, ip_address, hostname):
                 if instance.get('ipv6_ula') != str(ip_address):
                     instance['ipv6_ula'] = str(ip_address) # Unique local
                     updated = True
-        elif type(ip_address) is ipaddress.IPv4Address:
+        elif isinstance(ip_address, ipaddress.IPv4Address):
             if instance.get('ipv4') != str(ip_address):
                 instance['ipv4'] = str(ip_address)
                 updated = True
-        
+
         # If another instance has the same IP address then no longer use it for that instance
         ip_address_fields = ['ipv4', 'ipv6_gua', 'ipv6_ula']
         for mac, ins in instances.items():
@@ -301,7 +311,7 @@ def update_instance(instances, mac_address, ip_address, hostname):
                     if ins_ip_address and ins_ip_address == instance.get(ip_address_field):
                         del ins[ip_address_field]
                         updated = True
-    
+
     return updated
 
 if __name__ == '__main__':
